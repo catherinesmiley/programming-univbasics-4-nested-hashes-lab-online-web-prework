30e68a9e6701ec85d7f389baafdc8f8e829b415b
Catherine Smiley <catherinesmiley@gmail.com>
2019-12-03 03:35:44 +0000

    Automatically backed up by Learn

--- nested.rb
+++ nested.rb
@@ -130,6 +130,8 @@ def adding_to_dennis
         }
      }
 
-programmer_hash[:dennis_ritchie][:languages] << 
+programmer_hash[:dennis_ritchie][:languages] << "Assembly"
+
+return programmer_hash
 
 end
